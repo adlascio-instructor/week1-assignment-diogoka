6a533287d3db409f3e09fa881d8155bb15293246
Diogo Krub de Almeida <diogokalmeida@gmail.com>
2023-03-22 08:48:50 -0700

    EX05 done.

--- exercises/ex05.js
+++ exercises/ex05.js
@@ -8,18 +8,31 @@ https://www.google.com/search?q=cornerstone%20college
 
 This URL will perform a google search for the term "cornerstone college". Notice that when the string "cornerstone college" is part of a URL, the space is replaced with %20.
 
-If we want to add a parameter to a url, there are certain characters that must be encoded in order to make the URL valid. There are many characters that must be encoded, including: , !, ", and #. For this exercise, we will only be focusing on replacing the space with %20.
+If we want to add a parameter to a url, there are certain characters that must be encoded in order to make the URL valid. 
+There are many characters that must be encoded, including: , !, ", and #. For this exercise, we will only be focusing on replacing the space with %20.
 
 
 Instruction
-Create a function named urlEncode that will receive a string of words, and return that string with all of the whitespace characters converted to %20. If there is whitespace on the outside of the string, like in our second example above, you should only replace the whitespace within the string.
+Create a function named urlEncode that will receive a string of words, and return that string with all of the whitespace characters converted to %20. 
+If there is whitespace on the outside of the string, like in our second example above, you should only replace the whitespace within the string.
 
 Warning
 Use some sort of looping. Do Not use String.prototype.replace
 */
 
 const urlEncode = function (text) {
-  // Put your solution here
+  const newString = text.trim();
+  let array = newString.split("");
+  let result = "";
+  array.forEach(element => {
+    if (element === " ") {
+      result += "%20";
+    } else {
+      result += element;
+    }
+  });
+
+  return result;
 };
 
 console.log(urlEncode("cornerstone college")); //cornerstone%20college
